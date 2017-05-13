093d2da45e28714a1ff5c4b70c8df89b1cd47d95
Niklas Fischer <n.fischer@reply.de>
2017-05-13 16:26:02 +0200

    setting geolocation may work?!

--- Selly/src/main/java/com/reply/hackaton/executors/GeoLocalizationSet.java
+++ Selly/src/main/java/com/reply/hackaton/executors/GeoLocalizationSet.java
@@ -1,17 +1,43 @@
 package com.reply.hackaton.executors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.reply.hackaton.model.IntentExecutor;
 import com.reply.hackaton.model.Response;
+import com.reply.hackaton.model.User;
+import com.reply.hackaton.repository.UserRepository;
 
 @Service("GeoLocalization_Set")
 public class GeoLocalizationSet implements IntentExecutor {
 
+	@Autowired
+	UserRepository users;
+
 	@Override
+	@Transactional
 	public String execute(Response ApiAIResponse) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!ApiAIResponse.getResult().isActionIncomplete())
+			return ApiAIResponse.getResult().getSpeech();
+
+		Iterable<User> iterable = users.findAll();
+		User resultUser = null;
+		for (User u : iterable) {
+			if (u.getPAN().equals("525500******9045")){
+				resultUser = u;
+				break;
+			}
+		}
+		assert(resultUser != null);
+		
+		if (ApiAIResponse.getResult().getResolvedQuery() == "Si"){
+			resultUser.setGeoFencing(ApiAIResponse.getResult().getParameters().get("ZoneN"));
+			return "Ok, I have enabled your card for " + ApiAIResponse.getResult().getParameters().get("ZoneN");
+		}
+		else {
+			return "Ok, I will not change the geo-fencing settings of your card.";
+		}
 	}
 
 }
